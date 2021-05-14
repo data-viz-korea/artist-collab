46597342de79af6114b34759f08b0f8ff1b2d03f
Chloe Noh <shwldus3@gmail.com>
2021-05-14 18:56:53 +0900

    insert billboard data to postgresql

---
+++ backend/scripts/insert_data_to_postgres.js
@@ -0,0 +1,102 @@
+const { Pool } = require('pg')
+const pool = new Pool({
+    host: 'localhost',
+    database: 'artist_collab',
+    port: 5432
+});
+
+const getSongData = () => {
+    const songJson = require('./data/songs_table.json');
+    const data = songJson.data;
+    const result = data.map(d => {
+        const text = 
+        `INSERT INTO song(
+            id, 
+            name, 
+            rank_min, 
+            rank_mean5, 
+            rank_mean, 
+            rank_median, 
+            weeks_on_chart, 
+            week_min, 
+            week_max, 
+            collaborator_ids
+            ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
+        const values = [d.index, d.song_name, d.rank_min, d.rank_mean5, d.rank_mean, 
+            d.rank_median, d.weeks_on_chart, d.week_min, d.week_max, d.collaborators_id];
+    
+        return {
+            sql: text,
+            value: values
+        };
+    });
+    return result;
+}
+
+const getArtistData = () => {
+    const artistJson = require('./data/artists_table.json');
+    const data = artistJson.data;
+    const result = data.map(d => {
+        const text = 
+        `INSERT INTO artist(
+            id, 
+            name, 
+            week_min,
+            week_max,
+            weeks_on_chart_total, 
+            weeks_on_chart_longest, 
+            weeks_on_chart_mean, 
+            rank_min, 
+            rank_mean5, 
+            rank_mean, 
+            rank_median,
+            rank_min_mean,
+            rank_min_mean5,
+            rank_min_median,
+            num_songs,
+            y_min,
+            y_max 
+        ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)`;
+        const values = [d.index, d.artist_name, d.week_min, d.week_max, d.weeks_on_chart_total, 
+            d.weeks_on_chart_longest, d.weeks_on_chart_mean, d.rank_min, d.rank_mean5, d.rank_mean, 
+            d.rank_median, d.rank_min_mean, d.rank_min_mean5, d.rank_min_median, d.num_songs, 
+            d.y_min, d.y_max];
+
+        return {
+            sql: text,
+            value: values
+        };
+    });
+    return result;
+}
+
+const insertData = async (data) => {
+    const client = await pool.connect()
+
+    try {
+        const res = await client.query(data.sql, data.value)
+        // console.log(res.rows[0])
+    } finally {
+        client.release()
+    }
+}
+
+(async () => {
+    console.log('Start to insert artist data');
+    const artistData = getArtistData();
+    for (const d of artistData) {
+        console.log(d);
+        await insertData(d);
+    }
+    console.log(`Finish artist data >>> ${artistData.length}`);
+   
+    console.log('Start to insert song data');
+    const songData = getSongData();
+    for (const d of songData) {
+        console.log(d);
+        await insertData(d);
+    }
+    console.log(`Finish song data >>> ${songData.length}`);
+
+    await pool.end();
+})().catch(err => console.log(err))
---
+++ backend/sql/create_table.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS artist_collab;
+
+CREATE TABLE IF NOT EXISTS song  (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100),
+    rank_min INTEGER,
+    rank_mean5 DOUBLE PRECISION,
+    rank_mean DOUBLE PRECISION,
+    rank_median DOUBLE PRECISION,
+    weeks_on_chart DOUBLE PRECISION,
+    week_min TIMESTAMP,
+    week_max TIMESTAMP,
+    collaborator_ids INTEGER[],
+    applemusic_url VARCHAR(200),
+    spotify_url VARCHAR(200),
+    youtube_url VARCHAR(200)
+);
+
+
+CREATE TABLE IF NOT EXISTS artist (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100),
+    week_min TIMESTAMP,
+    week_max TIMESTAMP,
+    weeks_on_chart_total INTEGER,
+    weeks_on_chart_longest DOUBLE PRECISION,
+    weeks_on_chart_mean DOUBLE PRECISION,
+    rank_min INTEGER,
+    rank_mean5 DOUBLE PRECISION,
+    rank_mean DOUBLE PRECISION,
+    rank_median DOUBLE PRECISION,
+    rank_min_mean5 DOUBLE PRECISION,
+    rank_min_mean DOUBLE PRECISION,
+    rank_min_median DOUBLE PRECISION,
+    num_songs INTEGER,
+    y_min DOUBLE PRECISION,
+    y_max DOUBLE PRECISION,
+    photo_url VARCHAR(200),
+    wiki_url VARCHAR(200)
+);
+
+
