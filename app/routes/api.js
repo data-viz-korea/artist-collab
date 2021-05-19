8a793889d6df5b6263f4344c1fb58d2f4a4ae601
Chloe Noh <shwldus3@gmail.com>
2021-05-19 23:10:07 +0900

    /api/data 추가

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/data', function(req, res, next) {
   const user = {
     "nodes": [
       {
